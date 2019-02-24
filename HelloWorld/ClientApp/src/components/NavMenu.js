88a10c48406e3c6faa2f6ed334af3a75e542f9f0
romandubinin <romandubinin94@gmail.com>
2019-02-24 11:40:45 +0500

    small bug in NavMenu

--- HelloWorld/ClientApp/src/components/NavMenu.js
+++ HelloWorld/ClientApp/src/components/NavMenu.js
@@ -40,7 +40,7 @@ export class NavMenu extends Component {
                   <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/ideom">Ideoms</NavLink>
+                  <NavLink tag={Link} className="text-dark" to="/ideoms">Ideoms</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
